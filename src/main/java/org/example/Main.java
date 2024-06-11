8f6b2fdaafb4627376f818c4b5942f4a729d7492
pc-629 <schedrovaekaterina@gmail.com>
2024-06-11 22:43:06 +0300

    *dz-7 - enum SolarSystem class added; - simple loop to check enum values added in main class.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dz-7");
+
+        // just a loop to check all constants =)
+        for (SolarSystem planet : SolarSystem.values()){
+            System.out.println("--------------------------------");
+            System.out.println(planet.getSerialNumber() + " - " + planet );
+            System.out.println("Previous Distance - " + planet.getPreviousDistance());
+            System.out.println("Distance from Sun - " + planet.getDistanceFromSun());
+            System.out.println("Radius - " + planet.getRadius());
+            System.out.println("Previous Planet - " + planet.getPrevious());
+            System.out.println("Next Planet - " + planet.getNext());
+        }
     }
 }
